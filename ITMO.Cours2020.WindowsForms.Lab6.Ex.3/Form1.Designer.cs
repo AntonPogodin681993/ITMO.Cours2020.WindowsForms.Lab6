4478dc81747156a9633cfc25d5e6f5cb3ddb5e31
Anton Pogodin <anton.pogodin68@bk.ru>
2020-05-30 13:48:04 +0300

    Add project files.

---
+++ ITMO.Cours2020.WindowsForms.Lab6.Ex.3/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace ITMO.Cours2020.WindowsForms.Lab6.Ex._3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblA = new System.Windows.Forms.Label();
+            this.lblB = new System.Windows.Forms.Label();
+            this.txbA = new System.Windows.Forms.TextBox();
+            this.txbB = new System.Windows.Forms.TextBox();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.btnWork = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblA
+            // 
+            this.lblA.AutoSize = true;
+            this.lblA.Location = new System.Drawing.Point(12, 9);
+            this.lblA.Name = "lblA";
+            this.lblA.Size = new System.Drawing.Size(86, 17);
+            this.lblA.TabIndex = 0;
+            this.lblA.Text = "Значение А";
+            // 
+            // lblB
+            // 
+            this.lblB.AutoSize = true;
+            this.lblB.Location = new System.Drawing.Point(212, 9);
+            this.lblB.Name = "lblB";
+            this.lblB.Size = new System.Drawing.Size(86, 17);
+            this.lblB.TabIndex = 1;
+            this.lblB.Text = "Значение В";
+            // 
+            // txbA
+            // 
+            this.txbA.Location = new System.Drawing.Point(104, 6);
+            this.txbA.Name = "txbA";
+            this.txbA.Size = new System.Drawing.Size(100, 22);
+            this.txbA.TabIndex = 2;
+            // 
+            // txbB
+            // 
+            this.txbB.Location = new System.Drawing.Point(304, 9);
+            this.txbB.Name = "txbB";
+            this.txbB.Size = new System.Drawing.Size(100, 22);
+            this.txbB.TabIndex = 3;
+            // 
+            // btnRun
+            // 
+            this.btnRun.Location = new System.Drawing.Point(12, 52);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(75, 29);
+            this.btnRun.TabIndex = 4;
+            this.btnRun.Text = "Сумма";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // btnWork
+            // 
+            this.btnWork.Location = new System.Drawing.Point(180, 102);
+            this.btnWork.Name = "btnWork";
+            this.btnWork.Size = new System.Drawing.Size(75, 28);
+            this.btnWork.TabIndex = 5;
+            this.btnWork.Text = "Работа";
+            this.btnWork.UseVisualStyleBackColor = true;
+            this.btnWork.Click += new System.EventHandler(this.btnWork_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(407, 153);
+            this.Controls.Add(this.btnWork);
+            this.Controls.Add(this.btnRun);
+            this.Controls.Add(this.txbB);
+            this.Controls.Add(this.txbA);
+            this.Controls.Add(this.lblB);
+            this.Controls.Add(this.lblA);
+            this.Name = "Form1";
+            this.Text = "Асинхронный запуск";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblA;
+        private System.Windows.Forms.Label lblB;
+        private System.Windows.Forms.TextBox txbA;
+        private System.Windows.Forms.TextBox txbB;
+        private System.Windows.Forms.Button btnRun;
+        private System.Windows.Forms.Button btnWork;
+
+        private delegate int AsyncSumm(int a, int b);
+
+        private int Summ(int a, int b)
+        {
+            System.Threading.Thread.Sleep(9000);
+            return a + b;
+        }
+    }
+}
+
